1d96eb5983dd0194169d6509ec6daf0e7ae0120d
vparkhomchuk <Vladimir.Parkhomchuk@sjrb.ca>
2017-05-09 23:55:51 -0600

    OPSIS. Add practice 6 (JavaScript, RegExp)

---
+++ Others/Opis/pract6/Practice6/practice6.js
@@ -0,0 +1,40 @@
+var IP_REGEXP = /^192.168.[0-9].(25[0-5]|2[0-4][0-9]|(1[0-9][0-9]))$/g;
+var TEXT_REGEXP = /^[A-Z]{2}[0-9]{4}$/g;
+
+function calcV() {
+    var ipValue = document.getElementById('ip').value;
+    var textValue = document.getElementById('text').value;
+
+    //validate IP field
+    if (validateInput(ipValue, IP_REGEXP)) {
+        console.log("IP Correct: " + ipValue);
+        document.getElementById('ipValidationIndicator').innerHTML = "<font color='green'>Correct</font>";
+    } else {
+        console.log("IP Wrong: " + ipValue);
+        document.getElementById('ipValidationIndicator').innerHTML = "<font color='red'>Wrong</font>";
+    }
+
+    //validate TEXT field
+    if (validateInput(textValue, TEXT_REGEXP)) {
+        console.log("Text Correct: " + textValue);
+        document.getElementById('textValidationIndicator').innerHTML = "<font color='green'>Correct</font>";
+    } else {
+        console.log("Text Wrong: " + textValue);
+        document.getElementById('textValidationIndicator').innerHTML = "<font color='red'>Wrong</font>";
+    }
+
+}
+
+function validateInput(str, reg) {
+    var res = str.search(reg);
+    console.log("res: " + res);
+    return res >= 0;
+}
+
+function clearFields() {
+    document.getElementById('ip').value = "";
+    document.getElementById('text').value = "";
+    document.getElementById('ipValidationIndicator').innerHTML = null;
+    document.getElementById('textValidationIndicator').innerHTML = null;
+}
+
